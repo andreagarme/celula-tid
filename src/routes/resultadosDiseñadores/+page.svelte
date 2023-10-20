2c7d6140c564d09d400d8ab1636c83b6ffa84952
Andreagarme <andreagarme@gmail.com>
2023-10-19 21:44:38 -0500

    Modificar página diseñadores

--- src/routes/resultadosDiseñadores/+page.svelte
+++ src/routes/resultadosDiseñadores/+page.svelte
@@ -2,88 +2,71 @@
 	import video from '$lib/assets/video/diseñadores.mp4';
 	import Banner from '$lib/components/Banner/banner.svelte';
 	import H3 from '$lib/components/H3/H3.svelte';
-	import H4 from '$lib/components/H4/H4.svelte';
-	import Resultados from '$lib/components/Resultados/Resultados.svelte';
-	import Icon from '@iconify/svelte';
-	import exceljs from 'exceljs';
-	import { Accordion, AccordionItem, Alert, Button, Fileupload, Spinner } from 'flowbite-svelte';
+	import type { Categoria } from '$lib/types/categoria';
+	import type { Diseñador, DiseñadorTitulos } from '$lib/types/diseñador';
+	import type { Evaluador, EvaluadorCategorias } from '$lib/types/evaluador';
+	import type { Feedback } from '$lib/types/feedback';
+	import type { Nivel } from '$lib/types/nivel';
+	import { calcularNivel } from '$lib/utils/calcular-nivel';
+	import { capitalizar } from '$lib/utils/capitalizar';
+	import { agruparPor } from '$lib/utils/group-by';
+	import { importarXlsx } from '$lib/utils/importar-xlsx';
+	import { normalizar } from '$lib/utils/normalize';
+	import { quitarClaves } from '$lib/utils/quitar-claves';
+	import { Accordion, AccordionItem, Alert, Badge, Fileupload } from 'flowbite-svelte';
 	import { feedbackDiseñadores } from './diseñadores';
 
-	let selectedColor = 'blue'; // Valor predeterminado
-	let isEmbedded = true; // Valor predeterminado
-	let isLoading = false;
+	type DiseñadorConNiveles = Diseñador & {
+		nivelAlcance: Nivel;
+		nivelAporte: Nivel;
+		nivelCalidad: Nivel;
+		puntajeAlcance: number;
+		puntajeAporte: number;
+		puntajeCalidad: number;
+		nivelTotal: Nivel;
+	};
+	type DatosDiseñador = {
+		nombreEmpleado: string;
+		evaluadores: DiseñadorConNiveles[];
+	};
 
-	let resultado: Record<string, any>[] = [];
-	let empleadosImportados: Record<string, any>[] = [];
-	let lideresImportados: Record<string, any>[] = [];
-	let tercerosImportados: Record<string, any>[] = [];
-	let resultadosFinales: Record<string, any>[] = [];
+	let categorias: EvaluadorCategorias;
+	let titulos: DiseñadorTitulos;
+	let diseñadores: DatosDiseñador[] = [];
 
-	let modalFeedback: {
-		pregunta: string;
-		puntaje: number;
-		color: 'red' | 'yellow' | 'green' | 'blue';
-		descripcion: string;
-	}[] = [];
-	let modalFeedbackAbierto = false;
-
-	/**
-	 * Función asincrónica para procesar los resultados de un evento de entrada, típicamente desencadenado por una carga de archivo.
-	 *
-	 * Esta función lee y analiza un archivo Excel cargado, extrae datos y los categoriza según el campo 'Rol'.
-	 * Luego, establece ciertas variables globales y actualiza el estado de un modal.
-	 *
-	 * @async
-	 * @param {Event} event - El evento de entrada desencadenado por la selección de un archivo.
-	 *
-	 * @throws {Error} Lanza un error si hay algún problema durante el procesamiento.
-	 */
-	async function procesarResultados(event: Event) {
-		// Extraer la propiedad 'files' del target del evento, asumiendo que es un HTMLInputElement.
-		const { files } = event.target as HTMLInputElement;
-
-		if (!files) return;
-
-		isLoading = true;
-
+	async function procesarArchivoXlsx(event: Event) {
 		try {
-			const libro = new exceljs.Workbook();
-			await libro.xlsx.load(await files[0].arrayBuffer());
-			const hoja = libro.worksheets[0];
+			const { files } = event.target as HTMLInputElement;
 
-			hoja.eachRow((row, rowNumber) => {
-				const rowValue: Record<string, any> = {};
+			if (!files || files.length === 0) return;
 
-				row.eachCell((cell, colNumber) => {
-					rowValue[hoja.getRow(2).getCell(colNumber).value as string] = cell.value;
-				});
+			const datosImportados = await importarXlsx(files[0], { indexEncabezado: 2 });
 
-				if (rowNumber !== 2) {
-					resultado.push(rowValue);
-				}
-			});
+			if (datosImportados.length < 3) {
+				throw new Error('La plantilla debe contener datos para procesar');
+			}
 
-			empleadosImportados = resultado.filter((item) => item.rol?.toLowerCase() === 'empleado');
-			lideresImportados = resultado.filter((item) => item.rol?.toLowerCase() === 'lider');
-			tercerosImportados = resultado.filter((item) => item.rol?.toLowerCase() === 'tercero');
+			categorias = datosImportados[0] as EvaluadorCategorias;
+
+			titulos = quitarClaves(datosImportados[1] as DiseñadorTitulos, ['id', 'email', 'nombreEvaluador']);
+
+			const diseñadoresAgrupados = agruparPor<Diseñador>(datosImportados.slice(2) as Diseñador[], ({ nombreEmpleado }) =>
+				capitalizar(normalizar(nombreEmpleado)),
+			);
+
+			diseñadores = Object.entries(diseñadoresAgrupados).map(([nombreEmpleado, evaluadores]) => ({
+				...generarResultados({ nombreEmpleado, evaluadores }),
+			}));
 		} catch (error) {
-			console.log(error);
-
-			alert(`Error al procesar el archivo ${JSON.stringify(error)}`);
-		} finally {
-			isLoading = false;
+			alert(error);
 		}
 	}
 
-	function obtenerPreguntas() {
-		return Object.entries(resultado?.[1] ?? []).slice(3);
+	function obtenerRespuesta(evaluador: Evaluador, pregunta: string): string {
+		return evaluador[pregunta as keyof Evaluador];
 	}
 
-	function encontrarRespuesta(usuario: Record<string, any>, pregunta: string) {
-		return Object.entries(usuario).find((entry) => entry[0] === pregunta)?.[1];
-	}
-
-	function calcularPuntaje(respuesta: string) {
+	function calcularPuntaje(respuesta: string): number {
 		const puntajes: { [key: string]: number } = {
 			'actualmente no esta a su alcance hacerlo': 0,
 			'menos de lo esperado': 1,
@@ -92,34 +75,22 @@
 			'resultados excepcionales y disruptivos': 4,
 		};
 
-		const respuestaNormalizada = respuesta
-			.normalize()
-			.replace(/[\u0300-\u036f]/g, '')
-			.toLowerCase()
-			.trim();
-
-		return puntajes[respuestaNormalizada];
+		return puntajes[normalizar(respuesta).toLowerCase()];
 	}
 
-	function sumarPuntaje(usuario: Record<string, any>, categoria: 'APORTE' | 'CALIDAD' | 'ALCANCE') {
-		let suma = 0;
+	function sumarPuntajeEvaluador(opciones: { evaluador: Evaluador; categoria: Categoria }): number {
+		const preguntasCategoria = Object.entries(categorias ?? {})
+			.filter(([_, categoria]) => categoria === opciones.categoria)
+			.map(([pregunta]) => pregunta);
 
-		const grupo = Object.entries(resultado?.[0] ?? []).filter(([, value]) => value === categoria);
+		const preguntasEvaluador = Object.entries(opciones.evaluador ?? {})
+			.filter(([pregunta]) => preguntasCategoria.includes(pregunta))
+			.map(([_, respuesta]) => respuesta);
 
-		for (const [key] of grupo) {
-			suma += calcularPuntaje(usuario[key]);
-		}
-
-		return suma;
+		return preguntasEvaluador.reduce((total, respuestaActual) => total + calcularPuntaje(respuestaActual), 0);
 	}
 
-	function calcularNivel({
-		puntaje,
-		categoria,
-	}: {
-		puntaje: number;
-		categoria: 'APORTE' | 'CALIDAD' | 'ALCANCE';
-	}): 'Bajo' | 'Senior' | 'Expert' | undefined {
+	function calcularNivelPuntaje({ puntaje, categoria }: { puntaje: number; categoria: Categoria }): Nivel | undefined {
 		const nivelBajo = categoria === 'APORTE' ? [0, 7] : [0, 5];
 		const nivelSenior = categoria === 'APORTE' ? [8, 12] : [6, 9];
 		const nivelExpert = categoria === 'APORTE' ? [13, 16] : [10, 12];
@@ -129,21 +100,109 @@
 		if (puntaje >= nivelExpert[0] && puntaje <= nivelExpert[1]) return 'Expert';
 	}
 
-	function generarRetroalimentacion(usuario: Record<string, any>) {
-		modalFeedback = [];
+	function obtenerDatosEvaluador(evaluador: Evaluador): {
+		puntajeAporte: number;
+		puntajeCalidad: number;
+		puntajeAlcance: number;
+		nivelAporte: Nivel;
+		nivelCalidad: Nivel;
+		nivelAlcance: Nivel;
+		nivelTotal: Nivel;
+	} {
+		const puntajeAporte = sumarPuntajeEvaluador({ evaluador, categoria: 'APORTE' });
+		const puntajeCalidad = sumarPuntajeEvaluador({ evaluador, categoria: 'CALIDAD' });
+		const puntajeAlcance = sumarPuntajeEvaluador({ evaluador, categoria: 'ALCANCE' });
+		const nivelAporte = calcularNivelPuntaje({ puntaje: puntajeAporte, categoria: 'APORTE' })!;
+		const nivelCalidad = calcularNivelPuntaje({ puntaje: puntajeCalidad, categoria: 'CALIDAD' })!;
+		const nivelAlcance = calcularNivelPuntaje({ puntaje: puntajeAlcance as number, categoria: 'ALCANCE' })!;
+		const nivelTotal = calcularNivel(
+			(frecuencia) => frecuencia['Bajo'] === 1 && frecuencia['Senior'] === 1 && frecuencia['Expert'] === 1,
+			{
+				niveles: [nivelAporte, nivelCalidad, nivelAlcance],
+				nivelPorDefecto: 'Senior',
+			},
+		);
 
-		for (const [preguntaClave, pregunta] of obtenerPreguntas()) {
-			const puntaje = calcularPuntaje(usuario[preguntaClave]);
+		return {
+			puntajeAporte,
+			puntajeCalidad,
+			puntajeAlcance,
+			nivelAporte,
+			nivelCalidad,
+			nivelAlcance,
+			nivelTotal,
+		};
+	}
 
-			if (puntaje === 0 || puntaje === 1) {
-				modalFeedback.push({ pregunta, puntaje, color: 'red', descripcion: feedbackDiseñadores[preguntaClave][0] });
-			} else if (puntaje === 2) {
-				modalFeedback.push({ pregunta, puntaje, color: 'yellow', descripcion: feedbackDiseñadores[preguntaClave][1] });
-			} else if (puntaje === 3) {
-				modalFeedback.push({ pregunta, puntaje, color: 'green', descripcion: feedbackDiseñadores[preguntaClave][2] });
-			} else if (puntaje === 4) {
-				modalFeedback.push({ pregunta, puntaje, color: 'blue', descripcion: feedbackDiseñadores[preguntaClave][3] });
-			}
+	function calcularNivelFinal({ evaluadores }: { evaluadores: DiseñadorConNiveles[] }): { evaluador: Evaluador; nivel: Nivel } {
+		const x = evaluadores
+			.map((evaluador) => ({
+				...evaluador,
+				puntajeTotal: evaluador.puntajeAporte + evaluador.puntajeCalidad + evaluador.puntajeAlcance,
+			}))
+			.sort((a, b) => a.puntajeTotal - b.puntajeTotal);
+
+		const y = x[0].nivelTotal;
+
+		return {
+			evaluador: x[0],
+			nivel: y,
+		};
+	}
+
+	function generarResultados({
+		nombreEmpleado,
+		evaluadores,
+	}: {
+		nombreEmpleado: string;
+		evaluadores: Diseñador[];
+	}): DatosDiseñador {
+		const xyz: DatosDiseñador = {
+			nombreEmpleado: nombreEmpleado,
+			evaluadores: [],
+		};
+
+		for (const evaluador of evaluadores) {
+			const evaluadorConNiveles = { ...evaluador, ...obtenerDatosEvaluador(evaluador) };
+			xyz.evaluadores?.push(evaluadorConNiveles);
+		}
+
+		return xyz;
+	}
+
+	function generarRetroalimentacion(evaluador: Evaluador, pregunta: string): Feedback | undefined {
+		const puntaje = calcularPuntaje(evaluador[pregunta as keyof Evaluador]);
+
+		if (puntaje === 0 || puntaje === 1) {
+			return {
+				puntaje: puntaje,
+				descripcion: feedbackDiseñadores[pregunta][0],
+				color: 'red',
+			};
+		}
+
+		if (puntaje === 2) {
+			return {
+				puntaje,
+				descripcion: feedbackDiseñadores[pregunta][1],
+				color: 'yellow',
+			};
+		}
+
+		if (puntaje === 3) {
+			return {
+				puntaje,
+				descripcion: feedbackDiseñadores[pregunta][2],
+				color: 'green',
+			};
+		}
+
+		if (puntaje === 4) {
+			return {
+				puntaje,
+				descripcion: feedbackDiseñadores[pregunta][3],
+				color: 'blue',
+			};
 		}
 	}
 </script>
@@ -163,172 +222,144 @@
 			</p>
 		</div>
 	</div>
-
-	<div class="container mt-20 mb-24">
-		<Alert
-			class="max-w-5xl mx-auto prose prose-lg prose-stone dark:prose-invert"
-			color="blue"
-			dismissable
-		>
-			<span class="font-medium"> Bienvenidos!</span>
-			Para ver los resultados de Plan Carrera Diseñadores elige la plantilla correspondiente desde tu computador.
-		</Alert>
-
-		<div class="max-w-sm mx-auto mt-6">
-			<Fileupload on:change={procesarResultados} />
-		</div>
-
-		{#if isLoading}
-			<div class="grid mt-16 place-items-center">
-				<Spinner />
-			</div>
-		{:else}
-			{#if empleadosImportados.length || lideresImportados.length || tercerosImportados.length}
-				<section class="mt-16">
-					<H4 class="mb-4">Resultados por rol</H4>
-
-					<div class="max-w-4xl mx-auto">
-						{#each [empleadosImportados, lideresImportados, tercerosImportados] as usuarios, index}
-							<!-- <pre class="overflow-auto">{JSON.stringify(usuario)}</pre> -->
-							<!-- {@const puntajeAporte = sumarPuntaje(usuario, 'APORTE')}
-						{@const puntajeCalidad = sumarPuntaje(usuario, 'CALIDAD')}
-						{@const puntajeAlcance = sumarPuntaje(usuario, 'ALCANCE')} -->
-							{#if usuarios.length}
-								<div class="mt-3">
-									<Accordion>
-										<AccordionItem>
-											<svelte:fragment slot="header">
-												{#if index === 0}
-													Empleados
-												{:else if index === 1}
-													Líderes
-												{:else if index === 2}
-													Terceros
-												{/if}
-
-												({usuarios.length})
-											</svelte:fragment>
-
-											<Resultados
-												{usuarios}
-												tienePuntajeAporte
-												tienePuntajeCalidad
-												tienePuntajeAlcance
-											>
-												<svelte:fragment slot="actions">
-													<Button
-														color="alternative"
-														on:click={(event) => {
-															event.stopImmediatePropagation();
-														}}
-													>
-														<Icon
-															icon="solar:eye-outline"
-															class="mr-2 text-xl"
-														/>
-
-														Ver retroalimentación
-													</Button>
-												</svelte:fragment>
-											</Resultados>
-										</AccordionItem>
-									</Accordion>
-								</div>
-							{/if}
-						{/each}
-					</div>
-				</section>
-			{/if}
-
-			{#if resultadosFinales.length}
-				<section class="mt-12">
-					<H4 class="mb-4">Resultados finales</H4>
-
-					<Accordion>
-						<AccordionItem>
-							<svelte:fragment slot="header">Resultados finales</svelte:fragment>
-
-							<Resultados
-								usuarios={resultadosFinales}
-								tienePuntajeAporte
-								tienePuntajeCalidad
-								tienePuntajeAlcance
-								tieneNivelAporte
-								tieneNivelCalidad
-								tieneNivelAlcance
-								tieneNivelFinal
-							/>
-						</AccordionItem>
-					</Accordion>
-				</section>
-			{/if}
-		{/if}
-	</div>
 </div>
-<!-- 
-<Modal
-	title="Retroalimentación"
-	bind:open={modalFeedbackAbierto}
-	autoclose
->
-	<div class="flex flex-col gap-4">
-		{#each modalFeedback as { pregunta, puntaje, color, descripcion }, index}
-			<Alert
-				class="border-t-4"
-				{color}
-			>
-				<h4 class="font-semibold">{index + 1}. {pregunta}</h4>
-
-				<p class="mt-2">{descripcion}</p>
-
-				<div class="flex items-center justify-between mt-2">
-					<span class="font-semibold">
-						Puntaje: {puntaje}
-					</span>
-				</div>
-			</Alert>
-		{/each}
-	</div>
-
-	<svelte:fragment slot="footer">
-		<Button on:click={() => (modalFeedbackAbierto = false)}>Aceptar</Button>
-	</svelte:fragment>
-</Modal>
-
-<form>
-	<div class="container max-w-5xl mx-auto prose prose-lg prose-stone dark:prose-invert">
-		<H6>Elige los Textos de Retroalimentación</H6>
-		<Textarea
-			class="mt-10 mb-24"
-			placeholder="Write a comment"
-		>
-			<div
-				slot="footer"
-				class="flex items-center justify-between"
-			>
-				<Button type="submit">Post comment</Button>
-			</div>
-		</Textarea>
-	</div>
-</form>
-
-<form on:submit={handleSubmit}>
-	<label for="colorSelect">Selecciona un color:</label>
-	<select
-		id="colorSelect"
-		bind:value={selectedColor}
+<div class="container my-12">
+	<Alert
+		class="max-w-5xl mx-auto prose prose-lg prose-stone dark:prose-invert"
+		color="blue"
+		dismissable
 	>
-		<option value="blue">Azul</option>
-		<option value="red">Rojo</option>
-		<option value="green">Verde</option>
-	</select>
+		<span class="font-medium"> Bienvenidos!</span>
+		Para ver los resultados de Plan Carrera Diseñadores elige la plantilla correspondiente desde tu computador.
+	</Alert>
+	<div class="max-w-sm mx-auto mt-6">
+		<Fileupload on:change={procesarArchivoXlsx} />
+	</div>
 
-	<label>
-		<input
-			type="checkbox"
-			bind:checked={isEmbedded}
-		/> Embebido
-	</label>
+	{#if diseñadores}
+		<div class="mt-12">
+			<Accordion multiple>
+				{#each diseñadores as diseñador}
+					{@const nivelFinal = calcularNivelFinal({ evaluadores: diseñador.evaluadores })}
 
-	<button type="submit">Guardar</button>
-</form> -->
+					<AccordionItem>
+						<svelte:fragment slot="header">
+							{diseñador.nombreEmpleado} ({diseñador.evaluadores.length} evaluadores)
+						</svelte:fragment>
+
+						{#each diseñador.evaluadores as evaluador}
+							<Accordion flush>
+								<AccordionItem>
+									<svelte:fragment slot="header">
+										<div class="flex items-center justify-between w-full pr-4">
+											<div class="flex flex-col">
+												<span class="flex items-center gap-2">
+													{evaluador.nombreEvaluador}
+													<Badge color="dark">{evaluador.rol}</Badge>
+												</span>
+
+												<div class="mt-2">
+													<ul
+														class="text-sm font-normal list-disc md:list-none md:flex md:items-center md:gap-2 md:flex-wrap"
+													>
+														<li>
+															Total Aporte: {evaluador.puntajeAporte}
+															<span class="font-light">({evaluador.nivelAporte})</span>
+															<span class="hidden md:inline ml-1.5">•</span>
+														</li>
+
+														<li>
+															Total Calidad: {evaluador.puntajeCalidad}
+															<span class="font-light">({evaluador.nivelCalidad})</span>
+															<span class="hidden md:inline ml-1.5">•</span>
+														</li>
+
+														<li>
+															Total Alcance: {evaluador.puntajeAlcance}
+															<span class="font-light">({evaluador.nivelAlcance})</span>
+														</li>
+
+														<li>
+															Total Puntaje: {evaluador.puntajeAporte +
+																evaluador.puntajeCalidad +
+																evaluador.puntajeAlcance}
+														</li>
+													</ul>
+												</div>
+											</div>
+
+											<Badge color="green">
+												Nivel: {evaluador.nivelTotal}
+											</Badge>
+										</div>
+									</svelte:fragment>
+
+									{#each Object.entries(titulos) as [pregunta, titulo], index}
+										{@const respuesta = obtenerRespuesta(evaluador, pregunta)}
+										{@const puntaje = calcularPuntaje(respuesta)}
+
+										<div class="mt-6">
+											<h3 class="font-semibold">
+												{index + 1}. {titulo}
+											</h3>
+
+											<p class="my-1">{respuesta}</p>
+
+											{#if Number.isInteger(puntaje)}
+												<Badge color="green">
+													Puntaje: {puntaje}
+												</Badge>
+											{/if}
+										</div>
+									{/each}
+								</AccordionItem>
+							</Accordion>
+						{/each}
+
+						<div class="mt-6">
+							<Accordion>
+								<AccordionItem
+									defaultClass="flex items-center justify-between w-full font-medium text-left group-first:rounded-t-xl border-gray-200 dark:border-gray-700 bg-primary-200 dark:bg-primary-900"
+								>
+									<svelte:fragment slot="header">
+										<div class="flex items-center justify-between w-full pr-4">
+											<div class="flex flex-col">
+												<span class="flex items-center gap-2">
+													{nivelFinal.evaluador.nombreEvaluador}
+												</span>
+											</div>
+
+											<Badge color="green">
+												Nivel: {nivelFinal.nivel}
+											</Badge>
+										</div>
+									</svelte:fragment>
+
+									{#each Object.entries(titulos).filter( ([pregunta]) => pregunta.startsWith('pregunta'), ) as [pregunta, titulo], index}
+										{@const feedback = generarRetroalimentacion(nivelFinal.evaluador, pregunta)}
+
+										{#if feedback}
+											<div class="mt-2">
+												<Alert color={feedback.color}>
+													<h3 class="font-semibold">
+														{index + 1}. {titulo}
+													</h3>
+
+													<p class="my-2">{feedback.descripcion}</p>
+
+													<Badge color={feedback.color}>
+														Puntaje: {feedback.puntaje}
+													</Badge>
+												</Alert>
+											</div>
+										{/if}
+									{/each}
+								</AccordionItem>
+							</Accordion>
+						</div>
+					</AccordionItem>
+				{/each}
+			</Accordion>
+		</div>
+	{/if}
+</div>
